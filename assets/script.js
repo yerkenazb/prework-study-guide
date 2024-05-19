2fb738f056806809b4a8af2a2b2acf05a9b3c36e
yerkenazbayetova <Ybayetova>
2024-05-19 11:24:28 -0400

    adding code with for lopp

---
+++ assets/script.js
@@ -0,0 +1,26 @@
+
+const topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+
+for(let x=0; x<topics.length; x++){
+    console.log(topics[x]);
+}
+
+
+/*
+if (topic === 'HTML') {
+  console.log("Let's study HTML!");
+} else if (topic === 'CSS') {
+  console.log("Let's study CSS!");
+} else if (topic === 'Git') {
+  console.log("Let's study Git!");
+} else if (topic === 'JavaScript') {
+  console.log("Let's study JavaScript!");
+} else {
+  console.log('Please try again!');
+}
+
+const shapes = ["triangle", "square", "pentagon", "circle"];
+for(let x = 0; x < shapes.length; x++) {
+    console.log(shapes[x]);
+   }
+   */
